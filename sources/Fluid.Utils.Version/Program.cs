031431c4b000404ba5db3e6300151e3840ed49c7
Khindikaynen Egor <ambertape.music@gmail.com>
2020-04-05 18:34:43 +0300

    FLUID-70 Utils: Added version util.

---
+++ sources/Fluid.Utils.Version/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Fluid.Utils.Version
+{
+    class Program
+    {
+        private const string NugetVersionKey = "{$NUGETVERSION}";
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 2)
+                    throw new Exception("Fluid Version Tool: Input arguments not valid.");
+                
+                var path = args[0];
+                var version = args[1];
+
+                var files = Directory.GetFiles(path, "*.nuspec", SearchOption.TopDirectoryOnly);
+
+                foreach (var file in files)
+                {
+                    var text = File.ReadAllText(file);
+                    text = text.Replace(NugetVersionKey, version);
+                    File.WriteAllText(file, text);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
