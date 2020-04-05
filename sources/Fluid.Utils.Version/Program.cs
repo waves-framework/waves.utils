7ab6d8d67d7bbf005ccfc983930a8d11d9fede1e
Khindikaynen Egor <ambertape.music@gmail.com>
2020-04-05 20:53:51 +0300

    FLUID-70 Utils: Update version util.

--- sources/Fluid.Utils.Version/Program.cs
+++ sources/Fluid.Utils.Version/Program.cs
@@ -11,8 +11,10 @@ namespace Fluid.Utils.Version
         {
             try
             {
+                Console.WriteLine("[Fluid Version Tool] {0}", "Started.");
+                
                 if (args.Length != 2)
-                    throw new Exception("Fluid Version Tool: Input arguments not valid.");
+                    throw new Exception("[Fluid Version Tool]: Input arguments not valid.");
                 
                 var path = args[0];
                 var version = args[1];
@@ -21,10 +23,16 @@ namespace Fluid.Utils.Version
 
                 foreach (var file in files)
                 {
+                    Console.WriteLine("[Fluid Version Tool] {0}: {1}", "Updating file" , file);
+                
                     var text = File.ReadAllText(file);
                     text = text.Replace(NugetVersionKey, version);
                     File.WriteAllText(file, text);
+                    
+                    Console.WriteLine("[Fluid Version Tool] {0}: {1}", "File updated" , file);
                 }
+                
+                Console.WriteLine("[Fluid Version Tool] {0}", "Success.");
             }
             catch (Exception e)
             {
