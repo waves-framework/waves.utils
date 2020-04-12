2cc50e3efce2fcee17efa33fc58620926736a68b
Khindikaynen Egor <khek@ambertape.ru>
2020-04-12 19:15:29 +0300

    FLUID-71 Utils: Fix overwriting files.

--- sources/Fluid.Utils.Nuget.Packer/Program.cs
+++ sources/Fluid.Utils.Nuget.Packer/Program.cs
@@ -204,7 +204,7 @@ namespace Fluid.Utils.Nuget.Packer
 
                     Console.WriteLine("{0} {1}: {2}", ProgramName, InformationKey, "Copying template... (" + templateFileFullName + ").");
 
-                    File.Copy(templateFileFullName, nuspecFileFullName);
+                    File.Copy(templateFileFullName, nuspecFileFullName, true);
 
                     if (!File.Exists(nuspecFileFullName))
                     {
@@ -262,76 +262,11 @@ namespace Fluid.Utils.Nuget.Packer
                 }
                 catch (Exception e)
                 {
-                    Console.WriteLine("{0} {1}: {2}", ProgramName, ErrorKey, "An error occurred while initializing the utility:\r\n" + e);
+                    Console.WriteLine("{0} {1}: {2}", ProgramName, ErrorKey, "An error occurred while packing files:\r\n" + e);
 
                     Environment.ExitCode = 200;
                 }
             }
         }
-
-        //
-
-        //static void Main(string[] args)
-        //{
-        //    try
-        //    {
-        //        var watch = new Stopwatch();
-        //        watch.Start();
-
-        //        Console.WriteLine("[Fluid Version Tool] {0}", "Started.");
-
-        //        if (args.Length != 2)
-        //            throw new Exception("[Fluid Version Tool]: Input arguments not valid.");
-
-        //        var path = args[0];
-        //        var version = args[1];
-
-        //        var files = Directory.GetFiles(path, "*.nuspec", SearchOption.TopDirectoryOnly);
-
-        //        foreach (var file in files)
-        //        {
-        //            Console.WriteLine("[Fluid Version Tool] {0}: {1}", "Updating file", file);
-
-        //            var hasChanges = false;
-
-        //            var lines = File.ReadAllLines(file);
-
-        //            for (var i = 0; i < lines.Length; i++)
-        //            {
-        //                if (lines[i].Contains(NugetVersionKey))
-        //                {
-        //                    lines[i] = lines[i].Replace(NugetVersionKey, version);
-        //                    hasChanges = true;
-        //                }
-        //            }
-
-        //            File.WriteAllLines(file, lines);
-
-        //            if (hasChanges)
-        //            {
-        //                Console.ForegroundColor = ConsoleColor.Green;
-        //                Console.WriteLine("[Fluid Version Tool] {0}: {1}", "File updated", file);
-        //                Console.ForegroundColor = ConsoleColor.Gray;
-        //            }
-        //            else
-        //            {
-        //                Console.ForegroundColor = ConsoleColor.Yellow;
-        //                Console.WriteLine("[Fluid Version Tool] {0}: {1}", "File not updated", file);
-        //                Console.ForegroundColor = ConsoleColor.Gray;
-        //            }
-        //        }
-
-        //        Console.WriteLine("[Fluid Version Tool] {0}", "Success.");
-
-        //        watch.Stop();
-
-        //        Console.WriteLine("[Fluid Version Tool] {0}", "Time ellapsed: " + watch.Elapsed.TotalSeconds + " sec.");
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Console.WriteLine(e);
-        //        throw;
-        //    }
-        //}
     }
 }
