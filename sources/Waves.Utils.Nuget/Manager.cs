28bc8bda152b8cbace19372c3a44c782e1455ddf
Egor Khindikaynen <khek@ambertape.ru>
2020-08-22 16:00:53 +0300

    WAVES-128 Utils: Added Nuget manager.

---
+++ sources/Waves.Utils.Nuget/Manager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Common;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+
+namespace Waves.Utils.Nuget
+{
+    /// <summary>
+    /// Manager for operations with NuGet.
+    /// </summary>
+    public class Manager
+    {
+        private string _defaultRepository = "https://api.nuget.org/v3/index.json";
+        
+        private ILogger _logger = NullLogger.Instance;
+        private SourceCacheContext _cache = new SourceCacheContext();
+        private SourceRepository _repository;
+
+        /// <summary>
+        /// Creates new instance of <see cref="Manager"/>.
+        /// </summary>
+        public Manager()
+        {
+            _repository = Repository.Factory.GetCoreV3(_defaultRepository);
+        }
+
+        /// <summary>
+        /// Gets last minor version of package.
+        /// </summary>
+        /// <param name="packageName">Package name.</param>
+        /// <returns>Version.</returns>
+        public async Task<NuGetVersion> GetLastPackageMinorVersion(string packageName)
+        { 
+            var resource = await _repository.GetResourceAsync<FindPackageByIdResource>();
+            
+            var versions = await resource.GetAllVersionsAsync(
+                packageName,
+                _cache,
+                _logger,
+                CancellationToken.None);
+
+            return versions.Last();
+        }
+    }
+}
---
+++ sources/Waves.Utils.Sandbox/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Waves.Utils.Nuget;
+
+namespace Waves.Utils.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new Manager();
+            var version = manager.GetLastPackageMinorVersion("Waves.Core").Result;
+        }
+    }
+}
